d6aa0ff24d4e223dcd79903d60f424e4a02675f2
Gustavo Félix <99498636+Pheliquiz@users.noreply.github.com>
2023-02-14 22:02:21 -0300

    14-02
    ultimos exercicios do dia 14-02

--- Exercicio6.java
+++ Exercicio6.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ads.aula3tb;
 
-/**
- *
- * @author alunos
- */
+import java.util.Scanner;
+
 public class Exercicio6 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite um número positivo: ");
+        int numero = sc.nextInt();
+        
+        if (numero > 0){
+            System.out.println("Raiz quadrada de " + numero+ " = "+ Math.pow(numero, 2));
+        } else {
+            System.out.println("Número inválido");
+        }
+        
+    }
     
 }
